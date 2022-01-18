fe3ac59fd5dfcc9166096857647945b6fdac966a
Lanhild <archlan@protonmail.com>
2022-01-18 18:14:58 -0500

    new release

--- run_before_squashfs.sh
+++ run_before_squashfs.sh
@@ -67,6 +67,27 @@ rm "/boot/intel-ucode.img"
 rm "/boot/vmlinuz-linux"
 #rm "/var/log/pacman.log"
 # custom fixes currently needed:
+# Remove the useless gnome backgrounds
+gndir='/usr/share/backgrounds/gnome'
+if [[ -d "$gndir" ]]; then
+    rm -rf "$gndir"
+fi
+
+# Remove unnecessary .desktop entries - not presently working
+#adir="/usr/share/applications"
+#apps=(avahi-discover.desktop bssh.desktop bvnc.desktop echomixer.desktop \
+#	envy24control.desktop exo-preferred-applications.desktop feh.desktop \
+#	hdajackretask.desktop hdspconf.desktop hdspmixer.desktop hwmixvolume.desktop lftp.desktop \
+#	libfm-pref-apps.desktop lxshortcut.desktop lstopo.desktop \
+#	networkmanager_dmenu.desktop nm-connection-editor.desktop \
+#	qv4l2.desktop qvidcap.desktop stoken-gui.desktop stoken-gui-small.desktop thunar-bulk-rename.desktop \
+#	thunar-settings.desktop thunar-volman-settings.desktop yad-icon-browser.desktop)
+#
+#for app in "${apps[@]}"; do
+#	if [[ -e "$adir/$app" ]]; then
+#		sed -i '$s/$/\nNoDisplay=true/' "$adir/$app"
+#	fi
+#done
 ############################
 # end chrooted commandlist #
 ############################
